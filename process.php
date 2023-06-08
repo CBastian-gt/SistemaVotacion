a7e93084aa50c7bc1ae88cbf0336dc41ff06f79f
Carlos Gallardo <cbastian.toro@gmail.com>
2023-06-08 11:31:16 -0400

    Update process.php

---
+++ process.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombre_apellido = $_POST["nombre_apellido"];
+$alias = $_POST["alias"];
+$rut = $_POST["rut"];
+$email = $_POST["email"];
+$region = $_POST["region"];
+$comuna = $_POST["comuna"];
+$candidato = $_POST["candidato"];
+$como_se_entero = $_POST["como_se_entero"];
