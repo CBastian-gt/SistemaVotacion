b2537cd1b747ba8e7921017cc4dda3e9fa211aa2
Carlos Gallardo <cbastian.toro@gmail.com>
2023-06-08 17:10:27 -0400

    Update form.js

--- JS/form.js
+++ JS/form.js
@@ -13,7 +13,26 @@ $( document ).ready(function() {
     });
 
     $('#region').on('change', function() {
-        console.log($(this).val());
+        var regionId = $(this).val();
+        // Realizar la solicitud AJAX para obtener las comunas
+        $.ajax({
+            url: 'process.php',
+            type: 'POST',
+            data: { region_id: regionId },
+            dataType: 'json',
+            success: function(response) {
+                // Actualizar las opciones del campo de selección de comuna
+                var options = response.options;
+                var html = '';
+                for (var i = 0; i < options.length; i++) {
+                    html += '<option value="' + options[i].value + '">' + options[i].label + '</option>';
+                }
+                $('#comunas').html(html);
+            },
+            error: function(xhr, status, error) {
+                console.log('Error al obtener las comunas: ' + error);
+            }
+        });
     });
-    
+
 });
