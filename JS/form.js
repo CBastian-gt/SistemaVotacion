64fc9a6227d2d6c86ad4ab1393e2c6fa03e91612
Carlos Gallardo <cbastian.toro@gmail.com>
2023-06-08 13:47:13 -0400

    Update form.js

--- JS/form.js
+++ JS/form.js
@@ -1,13 +1,11 @@
- 	
-// A $( document ).ready() block.
 $( document ).ready(function() {
     $("#votingForm").submit(function(e){
         e.preventDefault();
         $.ajax({
             data: $("#votingForm").serialize(),
-            url:   'process.php', //archivo que recibe la peticion
-            type:  'post', //método de envio
-            success:  function (response) { //una vez que el archivo recibe el request lo procesa y lo devuelve
+            url:   'process.php', 
+            type:  'post', 
+            success:  function (response) {
                 alert(response);
             }
         });
