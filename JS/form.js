c586f7a5418a54530950300927c1909ab7fe3c25
Carlos Gallardo <cbastian.toro@gmail.com>
2023-06-08 19:12:50 -0400

    Update form.js

--- JS/form.js
+++ JS/form.js
@@ -12,27 +12,31 @@ $( document ).ready(function() {
 
     });
 
+
     $('#region').on('change', function() {
         var regionId = $(this).val();
+        
         // Realizar la solicitud AJAX para obtener las comunas
         $.ajax({
-            url: 'process.php',
-            type: 'POST',
-            data: { region_id: regionId },
-            dataType: 'json',
-            success: function(response) {
-                // Actualizar las opciones del campo de selección de comuna
-                var options = response.options;
-                var html = '';
-                for (var i = 0; i < options.length; i++) {
-                    html += '<option value="' + options[i].value + '">' + options[i].label + '</option>';
-                }
-                $('#comunas').html(html);
-            },
-            error: function(xhr, status, error) {
-                console.log('Error al obtener las comunas: ' + error);
-            }
+          url: 'process.php',
+          type: 'POST',
+          data: { region_id: regionId },
+          dataType: 'json',
+          success: function(response) {
+            // Actualizar las opciones del campo de selección de comuna
+            var options = response.comunas.map(function(comuna) {
+              return '<option value="' + comuna.comuna_id + '">' + comuna.comuna_nombre + '</option>';
+            }).join('');
+            $('#comuna').html(options);
+            
+            // Habilitar el campo de selección de comuna
+            $('#comuna').prop('disabled', false);
+          },
+          error: function(xhr, status, error) {
+            console.log('Error al obtener las comunas: ' + error);
+          }
         });
     });
+
 
 });
