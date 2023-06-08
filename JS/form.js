8b744ef94fd4d3e5904231f7bdec74a4ca1b62a6
Carlos Gallardo <cbastian.toro@gmail.com>
2023-06-08 19:20:55 -0400

    Update form.js

--- JS/form.js
+++ JS/form.js
@@ -13,30 +13,35 @@ $( document ).ready(function() {
     });
 
 
+
     $('#region').on('change', function() {
         var regionId = $(this).val();
         
         // Realizar la solicitud AJAX para obtener las comunas
         $.ajax({
-          url: 'process.php',
-          type: 'POST',
-          data: { region_id: regionId },
-          dataType: 'json',
-          success: function(response) {
-            // Actualizar las opciones del campo de selección de comuna
-            var options = response.comunas.map(function(comuna) {
-              return '<option value="' + comuna.comuna_id + '">' + comuna.comuna_nombre + '</option>';
-            }).join('');
-            $('#comuna').html(options);
-            
-            // Habilitar el campo de selección de comuna
-            $('#comuna').prop('disabled', false);
-          },
-          error: function(xhr, status, error) {
-            console.log('Error al obtener las comunas: ' + error);
-          }
+            url: 'fetch_comunas.php',
+            type: 'POST',
+            data: { region_id: regionId },
+            dataType: 'json',
+            success: function(response) {
+                // Actualizar las opciones del campo de selección de comuna
+                var options = '';
+                if (response.success) {
+                    $.each(response.comunas, function(index, comuna) {
+                        options += '<option value="' + comuna.comuna_id + '">' + comuna.comuna_nombre + '</option>';
+                    });
+                }
+                $('#comuna').html(options);
+                
+                // Habilitar el campo de selección de comuna
+                $('#comuna').prop('disabled', false);
+            },
+            error: function(xhr, status, error) {
+                console.log('Error al obtener las comunas: ' + error);
+            }
         });
     });
+
 
 
 });
