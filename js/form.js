888b52cbd1d3a0f1716a6c2068f46929347af7a4
Carlos Gallardo <cbastian.toro@gmail.com>
2023-06-08 21:35:22 -0400

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,19 +1,26 @@
 $( document ).ready(function() {
+    // Se ejecuta cuando el documento está listo
     $("#votingForm").submit(function(e){
         e.preventDefault();
+        // Evita que el formulario se envíe de forma predeterminada
         $.ajax({
             data: $("#votingForm").serialize(),
             url:   'process.php', 
             type:  'post', 
             success:  function (response) {
                 alert(response);
+                // Muestra una alerta con la respuesta del servidor
+                
                 window.location.href = 'index.php';
+                // Redirecciona a index.php
             }
         });
     });
 
     $('#region').on('change', function() {
+        // Se ejecuta cuando hay un cambio en el elemento con ID "region"
         let regionId = $(this).val();
+        // Obtiene el valor seleccionado en el elemento "region"
         if (regionId == ""){
             $("#comuna").html('<option value="">Selecciona una comuna</option>');
             return;
@@ -24,9 +31,9 @@ $( document ).ready(function() {
             type:  'post', 
             success:  function (response) {
                 $("#comuna").html(response);
+                // Actualiza el contenido del elemento "comuna" con la respuesta del servidor
             }
         });
         
     });
-        
 });
