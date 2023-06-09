83ed1eee43c95dcb60637b9fea48142e49b30a5c
Carlos Gallardo <cbastian.toro@gmail.com>
2023-06-08 20:02:16 -0400

    Update comunas.php

--- comunas.php
+++ comunas.php
@@ -8,6 +8,17 @@ if (empty($region)) {
     exit;
 }
 
-
+$sql_comunas = "SELECT comuna_id, comuna_nombre
+                FROM comunas
+                INNER JOIN provincias On provincias.provincia_id = comunas.provincia_id
+                WHERE region_id = ". $region . " ORDER BY comuna_nombre";
+$result_comunas = $conn->query($sql_comunas);
+$html = '<option value="">Selecciona una comuna</option>';
+if ($result_comunas->num_rows > 0) {
+    while ($row = $result_comunas->fetch_assoc()) {
+        $html = $html . "<option value='" . $row['comuna_id'] . "'>" . $row['comuna_nombre'] . "</option>";
+    }
+}
+echo $html;
 
 ?>
